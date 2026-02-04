5988d9b7c6dbafe8352e35e1fa88cce72ace9a27
jcastro8dx <>
2026-02-04 01:09:19 +0000

    Challenge: Zip and Sort Functions

--- functions/challenge_zip_and_sort_functions/main.py
+++ functions/challenge_zip_and_sort_functions/main.py
@@ -7,7 +7,9 @@ prices = [1.20, 0.50, 2.50, 1.75]
 # List of quantity sold
 quantities_sold = [50, 100, 25, 40]
 
+# the zip function creates a list of tuples
 combined_list = zip(products, prices, quantities_sold)
+# sorted() sorts by product name in ascending order
 sorted_products = sorted(combined_list)
 
 for index in range(len(sorted_products)):
