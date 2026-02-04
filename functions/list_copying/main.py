feb3ecf077a0713722251a0e26f01e33abce6266
jcastro8dx <>
2026-02-04 01:50:07 +0000

    List copying

--- functions/list_copying/main.py
+++ functions/list_copying/main.py
@@ -4,8 +4,8 @@ product_prices = [1.50, 2.50, 3.00, 0.99, 2.30]
 def apply_discount(prices):
     prices_copy = prices.copy()
     for index in range(len(prices_copy)):
-        if prices_copy[index] > 2 :
-            prices_copy[index] = prices_copy[index] - prices_copy[index]*0,10
+        if prices_copy[index] > 2 : 
+            prices_copy[index] = prices_copy[index] * (1-0.10)
 
     return prices_copy
 
